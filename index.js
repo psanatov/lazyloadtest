8345f2e977dbfc2712a703d500564931de38cd11
Pavel Sanatov <psanatov@gmail.com>
2018-03-10 02:22:39 +0100

    Done Basic Setup

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express()
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
+// static files
+app.use(express.static('static'))
+
+
 app.get('/', (req, res) => res.render('index.html'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
