6fa223c69be10f6acb64aa847f882a584b99ef0e
Pavel Sanatov <psanatov@gmail.com>
2018-03-10 02:12:03 +0100

    view engine setup

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// views
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', (req, res) => res.render('index.html'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
